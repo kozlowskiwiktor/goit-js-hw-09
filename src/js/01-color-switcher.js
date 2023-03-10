37879eb57f83c5daf3a893260c2f3ec5b88a6206
Wiktor Kozłowski <kozlowskiwiktor92@gmail.com>
2023-03-10 19:31:19 +0100

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
 
+const start = document.querySelector('button[data-start]')
+const stop = document.querySelector('button[data-stop]')
+
+// const button = document.querySelector('button');
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+start.addEventListener('click', () => {
+    timerID = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+    }, 1000);
+    start.disabled = true;
+});
+
+stop.addEventListener('click', () => {
+    clearInterval(timerID);
+    console.log(`Lucky You! Your color number is ${getRandomHexColor()}`);
+    start.disabled = false;
+});
