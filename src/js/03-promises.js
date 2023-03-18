5d1f0a65e83f81d2f6db7c8b2ca9da991955db91
Wiktor Kozłowski <kozlowskiwiktor92@gmail.com>
2023-03-18 18:00:11 +0100

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import Notiflix from 'notiflix';
+
+let form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const formInput = {
+    delay: Number(form.delay.value),
+    step: Number(form.step.value),
+    amount: Number(form.amount.value),
+  };
+  for (let i = 1; i <= formInput.amount; i++) {
+    createPromise(i, formInput.delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    formInput.delay += formInput.step;
+  }
+})
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const newPromise = new Promise(callback);
+
+  function callback(resolve, reject) {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   }
+  return newPromise;
 }
